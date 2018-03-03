bbeebc03b66a93679e3224dd250f73fdfac26850
pbastek <piotrbastek@poczta.fm>
2018-03-03 13:09:47 +0100

    Kodilla-Tasks

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -20,16 +20,16 @@ public class TaskController {
         return new TaskDto((long)1, "test title", "test_content");
     }
 
-    @RequestMapping(value = "deleted-task", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/deleted-task", method = RequestMethod.DELETE)
     public void deleteTask(String taskId){
     }
 
-    @RequestMapping(value = "updated-task", method = RequestMethod.PUT)
+    @RequestMapping(value = "/updated-task", method = RequestMethod.PUT)
     public TaskDto updateTask(TaskDto taskDto){
         return new TaskDto((long)1, "Edited test title", "Test content");
     }
 
-    @RequestMapping(value = "created-task", method = RequestMethod.POST)
+    @RequestMapping(value = "/created-task", method = RequestMethod.POST)
     public void createTask(TaskDto taskDto){
     }
 }
