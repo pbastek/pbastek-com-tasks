dfb3cb6b7b2f0b62014cd5d28fa46aa3d62ed030
pbastek <piotrbastek@poczta.fm>
2018-03-06 00:49:39 +0100

    Zad. 14.4 TaskController

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,35 +1,35 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/tasks")
+@RequestMapping("/v1/task")
 public class TaskController {
 
-    @RequestMapping(value = "/managed-tasks", method = RequestMethod.GET)
+    @GetMapping("/tasks")
     public List<TaskDto> getTasks(){ return new ArrayList<>();
     }
 
-    @RequestMapping(value = "/taskId", method = RequestMethod.GET)
+    @GetMapping("/tasks/{Id}")
     public TaskDto getTask(String taskId){
         return new TaskDto((long)1, "test title", "test_content");
     }
 
-    @RequestMapping(value = "/deleted-task", method = RequestMethod.DELETE)
+    @DeleteMapping("/tasks")
     public void deleteTask(String taskId){
     }
 
-    @RequestMapping(value = "/updated-task", method = RequestMethod.PUT)
+    @PutMapping
     public TaskDto updateTask(TaskDto taskDto){
         return new TaskDto((long)1, "Edited test title", "Test content");
     }
 
-    @RequestMapping(value = "/created-task", method = RequestMethod.POST)
+    @PostMapping
     public void createTask(TaskDto taskDto){
     }
 }
+
